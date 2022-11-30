4a9b519ad70b57f375f003681a6fc2ccfada243b
s3limm <108307150+s3limm@users.noreply.github.com>
2022-11-30 14:19:14 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
